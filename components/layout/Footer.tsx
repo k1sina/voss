ae542f9f5302cb704f1059e02b4fa23136d7f90f
Keivan Sina <keivan.sina@gmail.com>
2025-04-16 10:57:17 +0200

    - Refactor code structure for improved readability and maintainability - Enhanced footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,109 +4,146 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "../ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full bg-gray-50 py-16 pb-8 mt-auto">
+    <footer
+      className="w-full mt-auto bg-cover bg-center pt-16 last"
+      style={{ backgroundImage: 'url("/images/map-bg.png")' }}
+    >
+      {/* Background Effect */}
+      <div className="grid grid-cols-1 md:grid-cols-12 h-96 -mb-96">
+        <div
+          className="flex flex-col md:col-span-6 relative bg-cover bg-center"
+          style={{ backgroundImage: 'url("/images/map-caption.svg")' }}
+        ></div>
+      </div>
+      {/* Footer Content */}
       <div className="max-w-7xl mx-auto px-5">
-        <div className="grid grid-cols-1 md:grid-cols-12 gap-10 mb-10">
-          <div
-            className="flex flex-col md:col-span-6 relative bg-cover bg-center"
-            style={{ backgroundImage: 'url("/images/map-caption.svg")' }}
-          >
-            <div className="bg-white rounded-lg shadow-md p-6 relative z-10 max-w-md">
-              <div className="mb-5">
-                <Image
-                  src="/logos/voss-energy-logo-with-slogan.svg"
-                  alt="Voss Energy"
-                  width={180}
-                  height={60}
-                  className="h-auto w-48"
-                />
-              </div>
-              <div className="flex items-center mb-5">
-                <Button className="mr-2">
+        <div className="grid grid-cols-1 md:grid-cols-12">
+          <div className="flex flex-col md:col-span-6">
+            <Card className="relative z-10 max-w-md -mt-16 p-0 shadow-md">
+              <CardContent className="p-6">
+                <div className="mb-5">
                   <Image
-                    src="/icons/phone.svg"
-                    alt="Phone"
-                    width={20}
-                    height={20}
-                    className="h-5 w-5"
+                    src="/logos/voss-energy-logo-with-slogan.svg"
+                    alt="Voss Energy"
+                    width={180}
+                    height={60}
+                    className="h-auto w-48"
                   />
-                </Button>
-                <Button className="mr-2">
-                  <Image
-                    src="/icons/envelope-simple.svg"
-                    alt="Email"
-                    width={20}
-                    height={20}
-                    className="h-5 w-5"
-                  />
-                </Button>
-                <Button className="mr-2">
-                  <Image
-                    src="/icons/variant78.svg"
-                    alt="Linkedin"
-                    width={20}
-                    height={20}
-                    className="h-5 w-5"
-                  />
-                </Button>
-                <Button className="mr-2">
-                  <Image
-                    src="/icons/variant82.svg"
-                    alt="Youtube"
-                    width={20}
-                    height={20}
-                    className="h-5 w-5"
-                  />
-                </Button>
-              </div>
-              <p className="text-gray-600 leading-relaxed">
-                <b>VOSS Energy GmbH</b> <br />
-                Admannshäger Damm 20 <br />
-                18211 Admannshagen-Bargeshagen
-                <br />
-                Landkreis Rostock
-              </p>
-            </div>
+                </div>
+                <div className="flex items-center mb-5">
+                  <Button className="mr-2" variant="outline">
+                    <Image
+                      src="/icons/phone.svg"
+                      alt="Phone"
+                      width={20}
+                      height={20}
+                      className="h-5 w-5"
+                    />
+                  </Button>
+                  <Button className="mr-2" variant="outline">
+                    <Image
+                      src="/icons/envelope-simple.svg"
+                      alt="Email"
+                      width={20}
+                      height={20}
+                      className="h-5 w-5"
+                    />
+                  </Button>
+                  <Button className="mr-2" variant="outline">
+                    <Image
+                      src="/icons/variant78.svg"
+                      alt="Linkedin"
+                      width={20}
+                      height={20}
+                      className="h-5 w-5"
+                    />
+                  </Button>
+                  <Button className="mr-2" variant="outline">
+                    <Image
+                      src="/icons/variant82.svg"
+                      alt="Youtube"
+                      width={20}
+                      height={20}
+                      className="h-5 w-5"
+                    />
+                  </Button>
+                </div>
+                <p className="text-gray-600 leading-relaxed">
+                  <b>VOSS Energy GmbH</b> <br />
+                  Admannshäger Damm 20 <br />
+                  18211 Admannshagen-Bargeshagen
+                  <br />
+                  Landkreis Rostock
+                </p>
+              </CardContent>
+            </Card>
           </div>
-
-          <div className="flex flex-col md:col-span-3">
-            <h3 className="text-xl font-semibold mb-5 text-gray-800">
-              DAS KÖNNTE SIE INTERESSIEREN
-            </h3>
-            <div>
-              <Button className="mr-1 mb-1">Fläche prüfen</Button>
-              <Button className="mr-1 mb-1">Kontakt</Button>
-              <Button className="mr-1 mb-1">Unser Projektablauf</Button>
-              <Button className="mr-1 mb-1">Investieren</Button>
-              <Button className="mr-1 mb-1">Projekte</Button>
-              <Button className="mr-1 mb-1">Antrieb & Anspruch</Button>
-              <Button className="mr-1 mb-1">News</Button>
-              <Button className="mr-1 mb-1">Faq</Button>
-              <Button className="mr-1 mb-1">Jobs</Button>
-            </div>
-          </div>
-
-          <div className="flex flex-col md:col-span-3">
-            <p className="text-gray-600 mb-2">
-              <b>VOSS Energy</b> steht für die qualifizierte Entwicklung
-              erneuerbarer Energieprojekte mit expliziter Orientierung auf
-              Eigenbetrieb und Standortentwicklung.
-            </p>
+          <div className="flex md:col-span-6 bg-background p-8 gap-8">
+            <Card className="border-0 shadow-none py-0 flex-1">
+              <CardHeader>
+                <CardTitle className="text-xl font-semibold mb-5 text-gray-800">
+                  DAS KÖNNTE SIE INTERESSIEREN
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="px-0">
+                <div>
+                  <Button variant="outline" className="mr-2 mb-2">
+                    Fläche prüfen
+                  </Button>
+                  <Button variant="outline" className="mr-2 mb-2">
+                    Kontakt
+                  </Button>
+                  <Button variant="outline" className="mr-2 mb-2">
+                    Unser Projektablauf
+                  </Button>
+                  <Button variant="outline" className="mr-2 mb-2">
+                    Investieren
+                  </Button>
+                  <Button variant="outline" className="mr-2 mb-2">
+                    Projekte
+                  </Button>
+                  <Button variant="outline" className="mr-2 mb-2">
+                    Antrieb & Anspruch
+                  </Button>
+                  <Button variant="outline" className="mr-2 mb-2">
+                    News
+                  </Button>
+                  <Button variant="outline" className="mr-2 mb-2">
+                    Faq
+                  </Button>
+                  <Button variant="outline" className="mr-2 mb-2">
+                    Jobs
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+            <Card className="border-0 shadow-none py-0 flex-1">
+              <CardContent className="px-0">
+                <p className="text-gray-600 mb-2">
+                  <b>VOSS Energy</b> steht für die qualifizierte Entwicklung
+                  erneuerbarer Energieprojekte mit expliziter Orientierung auf
+                  Eigenbetrieb und Standortentwicklung.
+                </p>
+              </CardContent>
+            </Card>
           </div>
         </div>
-
-        <div className="border-t border-gray-200 pt-5">
+      </div>
+      {/* Footer Bottom */}
+      <div className=" py-4 bg-gray-50 relative z-10">
+        <div className="max-w-7xl mx-auto px-5">
           <div className="flex justify-between">
-            <div className="flex justify-center items-center gap-3">
+            <div className="flex justify-center items-center gap-14">
               <Link href="/">Datenschutz</Link>
               <Link href="/">Impressum</Link>
             </div>
-            <div className="flex justify-center items-center gap-3">
+            <div className="flex justify-center items-center gap-8">
               <p className="text-gray-500 text-sm text-center">
                 Ein Unternehmen der VOSS Gruppe
               </p>
@@ -114,9 +151,9 @@ const Footer = () => {
                 <Image
                   src="/logos/voss-logo.svg"
                   alt="Voss Logo"
-                  width={60}
-                  height={20}
-                  className="h-10 w-auto"
+                  width={125}
+                  height={16}
+                  className="h-4 w-auto"
                   priority
                 />
               </Link>
